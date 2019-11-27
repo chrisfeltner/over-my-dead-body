b9e480dcc53d3daa17762d702a8bbe1d4720145a
Chris Feltner <chris.feltner@Knights.ucf.edu>
2019-11-27 15:12:44 -0500

    retry mongo connection on failure; Docker fixes

--- server/server.js
+++ server/server.js
@@ -5,13 +5,27 @@ const userRoute = require('./routes/users');
 const noteRoute = require('./routes/notes');
 
 const listeningPort = process.env.PORT || 5000;
-const databasePort = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
-mongoose.connect(databasePort);
+const databasePort = process.env.MONGODB_URI || 'mongodb://db-service:27017';
 
 if (databasePort == undefined || databasePort == null) {
   console.log("MONGODB_URI is missing")
   return 1;
 } 
+
+const connectWithRetry = () => {
+    return mongoose.connect(databasePort, (error) => {
+        if(error)
+        {
+            console.log("Error on initial MongoDB connection. Retrying in 5 sec.");
+            setTimeout(connectWithRetry, 5000);
+        }
+        else {
+            console.log("Initial MongoDB connection successful!")
+        }
+    });
+};
+
+connectWithRetry();
 
 const app = express();
 
