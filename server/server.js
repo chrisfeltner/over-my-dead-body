e1efde1da443ab931d0170af281211507b54e683
Chris Feltner <chris.feltner@Knights.ucf.edu>
2019-11-27 21:23:35 -0500

    Registration

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ connectWithRetry();
 
 const app = express();
 
-app.use(cors({origin: 'http://localhost:3000', credentials: true}))
+app.use(cors({origin: ['http://localhost:3000', 'http://frontend-service:3000'], credentials: true}))
 app.use(express.json());
 app.use(cookieParser());
 app.use('/users', userRoute);
