51867e5d9aae2d73489baf2070c1f608a86faa49
Chris Feltner <chris.feltner@Knights.ucf.edu>
2019-11-27 21:02:36 -0500

    fixed CORS issue ... finally

---
+++ frontend-web/src/utils/auth.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const setAuthToken = (token) => {
+    if(token) {
+        axios.default.headers.common["Authorization"] = token;
+    }
+    else {
+        delete axios.default.headers.common["Authorization"];
+    }
+}
+
+export default setAuthToken;
